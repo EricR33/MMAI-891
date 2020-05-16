8a9d039b606914f02edceaeaa6186d31ae9b082c
EricR33 <59517978+EricR33@users.noreply.github.com>
2020-05-15 21:55:55 -0400

    RNN Modelling

--- Modeling/Eric_Modelling_ML.py
+++ Modeling/Eric_Modelling_ML.py
@@ -4,17 +4,12 @@ import re
 import unidecode
 # import numpy as np
 import pandas as pd
-import tensorflow as tf
+# import tensorflow as tf
 from nltk.stem import WordNetLemmatizer
+from sklearn.feature_extraction.text import TfidfVectorizer
 # from nltk.corpus import stopwords
-from tensorflow.python.keras.preprocessing.text import Tokenizer
-# from tensorflow.python.keras.preprocessing.sequence import pad_sequences
 from sklearn.model_selection import train_test_split
-from keras.layers import SpatialDropout1D, LSTM, Dense
-from keras.models import Sequential
-from keras.layers.embeddings import Embedding
-from keras.callbacks import EarlyStopping
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 
 # pwd
 
@@ -119,82 +114,5 @@ print(X.iloc[0, :].essay_pre)
 
 X = pd.DataFrame(X['essay_pre'])
 
-# Finding the maximum words for an essay --> Can't get this work: It only works with series type pandas data
-# maximum = 0
-# for i in range(len(X)):
-#    if len(nltk.word_tokenize(X.iloc[i])) > maximum:
-#        maximum = len(nltk.word_tokenize(X.iloc[i]))
-# print(maximum)
-
-# Finding the minimum words for an essay
-# minimum = 1051
-# for row in range(len(X)):
-#    if len(nltk.word_tokenize(row)) < minimum:
-#        minimum = len(nltk.word_tokenize(row))
-# print(minimum)
-
-# Calculate the unique tokens by hand: --> Can't get this work: It only works with series type pandas data
-# vector = {}
-# X = pd.Series(X['essay_pre'])
-# for i in range(len(X)):
-#    for word in nltk.word_tokenize(X.iloc[i]):
-#        if word not in vector.keys(X.iloc[i]):
-#            vector[word] = 1
-
-# the output looks to be a size 13650 words
-
-# Delete the essays that have less than 200 words ~ approx. 20 essays
-
-
-# Setting up the Keras LSTM Network
-# the code below is taken from https://towardsdatascience.com/multi-class-text-classification-with-lstm-1590bee1bd17
-MAX_NB_WORDS = 5000
-MAX_SEQUENCE_LENGTH = 975
-
-tokenizer = Tokenizer(num_words=MAX_NB_WORDS, filters='!"#$%&()*+,-./:;<=>?@[\]^_`{|}~', lower=True)
-tokenizer.fit_on_texts(X['essay_pre'].values)
-word_index = tokenizer.word_index
-print('Found %s unique tokens.' % len(word_index))
-
-# there appears to be 11379 unique tokens
-
-# Padding the sentences
-X2 = tokenizer.texts_to_sequences(X['essay_pre'].values)
-X2 = tf.keras.preprocessing.sequence.pad_sequences(X2, maxlen=MAX_SEQUENCE_LENGTH)
-print('Shape of data tensor:', X2.shape)
-
-# Train/Test/Split:
-X_train, X_test, y_train, y_test = train_test_split(X2, y, test_size=0.10, random_state=42)
-print(X_train.shape, y_train.shape)
-print(X_test.shape, y_test.shape)
-
-# Building the model: --> Stuck on this
-EMBEDDING_DIM = 100
-
-model = Sequential()
-model.add(Embedding(MAX_NB_WORDS, EMBEDDING_DIM, input_length=X.shape[1]))
-model.add(SpatialDropout1D(0.2))
-model.add(LSTM(100, dropout=0.2, recurrent_dropout=0.2))
-model.add(Dense(13, activation='softmax'))
-model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-
-epochs = 5
-batch_size = 64
-
-history = model.fit(X_train, y_train, epochs=epochs, batch_size=batch_size, validation_split=0.1,
-                    callbacks=[EarlyStopping(monitor='val_loss', patience=3, min_delta=0.0001)])
-
-accr = model.evaluate(X_test, y_test)
-print('Test set\n  Loss: {:0.3f}\n  Accuracy: {:0.3f}'.format(accr[0], accr[1]))
-
-plt.title('Loss')
-plt.plot(history.history['loss'], label='train')
-plt.plot(history.history['val_loss'], label='test')
-plt.legend()
-plt.show()
-
-plt.title('Accuracy')
-plt.plot(history.history['acc'], label='train')
-plt.plot(history.history['val_acc'], label='test')
-plt.legend()
-plt.show()
+vectorizer = TfidfVectorizer(max_df=0.7, min_df=0.05, max_features=500, ngram_range=[1,3])
+dtm = vectorizer.fit_transform(X['essay_pre'])
