cd4fbfc892d36dc7578a5bb3f3ef8b443c2c290d
EricR33 <59517978+EricR33@users.noreply.github.com>
2020-05-17 11:23:48 -0400

    ML Modelling

--- Modeling/Eric_Modelling_ML.py
+++ Modeling/Eric_Modelling_ML.py
@@ -1,4 +1,5 @@
 import os
+import textstat
 # import nltk
 import re
 import unidecode
@@ -9,7 +10,9 @@ from nltk.stem import WordNetLemmatizer
 from sklearn.feature_extraction.text import TfidfVectorizer
 # from nltk.corpus import stopwords
 from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
 # import matplotlib.pyplot as plt
+from sklearn.metrics import mean_squared_error, r2_score
 
 # pwd
 
@@ -21,21 +24,15 @@ WORKBOOK_DIR = './courses/'
 
 # Import Data
 df = pd.read_csv(os.path.join(DATASET_DIR, "training_set_rel3.tsv"), sep='\t', encoding='ISO-8859-1')
-
 df.info()
 df.head()
 
-# Split X & Y
-X = pd.DataFrame(df['essay'])
-X = X.iloc[12253:]
-print(X.iloc[0])
-X.head()
+df1 = pd.DataFrame(df['essay'])
+df1 = df1.iloc[12253:]
+print(df1.iloc[0])
+df1.head()
 
-y = df['domain1_score']
-y = y.iloc[12253:]
-y.head()
-
-print(y.shape, X.shape)
+print(df1.shape)
 
 # stop_words = set(stopwords.words('english') + stopwords.word('french'))   # haven't used this below
 
@@ -107,13 +104,51 @@ def preprocess(x):
     return ' '.join(x)
 
 
-X['essay_pre'] = X['essay'].apply(preprocess)
+df1['essay_pre'] = df1['essay'].apply(preprocess)
 
-print(X.iloc[0, :].essay)
-print(X.iloc[0, :].essay_pre)
+print(df1.iloc[0, :].essay)
+print(df1.iloc[0, :].essay_pre)
 
-X = pd.DataFrame(X['essay_pre'])
+df1 = pd.DataFrame(df1['essay_pre'])
 
-vectorizer = TfidfVectorizer(max_df=0.7, min_df=0.05, max_features=500, ngram_range=[1,3])
-dtm = vectorizer.fit_transform(X['essay_pre'])
+vectorizer = TfidfVectorizer(max_df=0.5, min_df=0.05, max_features=500, ngram_range=[1,3])
+dtm = vectorizer.fit_transform(df1['essay_pre'])
 
+vectorizer.get_feature_names()
+
+bow_df = pd.DataFrame(dtm.toarray(), columns=vectorizer.get_feature_names(),index=df1.index)
+
+df1_bow = pd.concat([df1, bow_df], axis=1)
+print(df1_bow.shape)
+
+# Feature Engineering
+df1_bow['length'] = df1_bow['essay_pre'].apply(lambda x: len(x))
+df1_bow['syllable count'] = df1_bow['essay_pre'].apply(lambda x: textstat.syllable_count(x))
+df1_bow['flesch_reading_ease'] = df1_bow['essay_pre'].apply(lambda x: textstat.flesch_reading_ease(x))
+df1_bow = df1_bow.drop(['essay_pre'], axis=1)
+
+# Split X & Y From Dataframe
+y = df['domain1_score']
+y = y.iloc[12253:]
+X = df1_bow
+
+# Train Test Split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Model Creation
+regr = LinearRegression()
+regr.fit(X_train, y_train)
+
+# Make predictions using the testing set
+y_pred = regr.predict(X_test)
+
+# The coefficients
+print('Coefficients: \n', regr.coef_)
+
+# The mean squared error
+print('Mean squared error: %.2f'
+      % mean_squared_error(y_test, y_pred))
+
+# The coefficient of determination: 1 is perfect prediction
+print('Coefficient of determination: %.2f'
+      % r2_score(y_test, y_pred))
