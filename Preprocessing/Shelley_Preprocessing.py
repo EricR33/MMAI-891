3fed214768e704f72c19f452cf525129e10909a8
cshellinc <shelley@cshellconsulting.com>
2020-05-18 15:11:53 -0400

    Added some code that generates a Word Cloud from a list (single, clean column from a DF)

--- Preprocessing/Shelley_Preprocessing.py
+++ Preprocessing/Shelley_Preprocessing.py
@@ -84,3 +84,202 @@ for essay in train_essays:
             sentences += essay_to_sentences(essay, remove_stopwords = True)
 
 print(sentences[2])
+
+####################################################
+# CREATE A WORD CLOUD
+# disclaimer - warning: shelley code! :)
+# requires seaborn, unidecode, wordcloud, lxml, html5lib
+####################################################
+
+# Import Libraries
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+# import re for regular expression library
+import nltk
+import re
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+from nltk.stem import PorterStemmer
+from nltk.stem import LancasterStemmer
+import unidecode
+from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
+
+####################################################
+# LOAD YOUR FILE/DATA INTO A PANDAS DATAFRAME
+####################################################
+
+sl_df = pd.read_csv("/Users/shelley/Documents/ShelleyLI_CSV.csv")
+#print(sl_df.info())
+#print(sl_df.head())
+
+
+#### testing code only IGNORE ####
+from bs4 import BeautifulSoup
+
+html = "This is a test"
+soup = BeautifulSoup(html,"html.parser")
+text = soup.get_text(strip=True)
+print (text)
+
+####################################################
+# CLEAN YOUR DATA
+####################################################
+
+from nltk.corpus import stopwords
+from bs4 import BeautifulSoup
+
+stop_words = set(stopwords.words('english'))
+stop_words.add('for')
+stop_words.add('see')
+###################################################
+# REMOVED EXTRA WORDS
+####################################################
+
+stop_words.add('endorsement')
+stop_words.add('endorsements')
+stop_words.add('addition')
+stop_words.add('please')
+stop_words.add('resume')
+stop_words.add('individual')
+stop_words.add('filled')
+stop_words.add('told')
+stop_words.add('spent')
+stop_words.add('find')
+stop_words.add('divisional')
+stop_words.add('would')
+stop_words.add('didnt')
+stop_words.add('etc')
+stop_words.add('undergraduate')
+stop_words.add('last')
+stop_words.add('entire')
+stop_words.add('go')
+stop_words.add('school')
+stop_words.add('department')
+stop_words.add('bachelor')
+stop_words.add('divisional')
+stop_words.add('role')
+stop_words.add('smith')
+stop_words.add('even')
+stop_words.add('together')
+stop_words.add('evening')
+stop_words.add('enclosed')
+stop_words.add('full')
+stop_words.add('part')
+stop_words.add('direct')
+stop_words.add('basis')
+stop_words.add('including')
+stop_words.add('perfect')
+stop_words.add('large')
+stop_words.add('time')
+stop_words.add('desktop')
+stop_words.add('drove')
+stop_words.add('company')
+stop_words.add('take')
+stop_words.add('high')
+stop_words.add('coupled')
+stop_words.add('many')
+stop_words.add('interim')
+stop_words.add('background')
+stop_words.add('year')
+stop_words.add('know')
+stop_words.add('hands')
+stop_words.add('candidate')
+stop_words.add('career')
+stop_words.add('add')
+stop_words.add('cornerstone')
+stop_words.add('earned')
+stop_words.add('degree')
+stop_words.add('brand')
+stop_words.add('aspect')
+stop_words.add('following')
+stop_words.add('second')
+stop_words.add('application')
+stop_words.add('received')
+stop_words.add('next')
+stop_words.add('someone')
+stop_words.add('honor')
+stop_words.add('dtype')
+stop_words.add('sent_clean')
+stop_words.add('object')
+stop_words.add('Length')
+stop_words.add('Name')
+
+lemmer = WordNetLemmatizer()
+porter = PorterStemmer()
+lancaster = LancasterStemmer()
+
+
+def preprocess(x):
+    # Remove HTML tags
+    x = BeautifulSoup(x,"html.parser").get_text(strip=True)
+
+    # Lower case
+    x = x.lower()
+
+    # Remove punctuation
+    x = re.sub(r'[^\w\s]', '', x)
+
+    # Remove non-unicode
+    x = unidecode.unidecode(x)
+
+    # Remove numbers
+    x = re.sub(r'\d+', '', x)
+
+    # Remove stopwords and lemmatize
+    x = [lemmer.lemmatize(w) for w in x.split() if w not in stop_words]
+
+    # Stemming
+    # x = [porter.stem(w) for w in x.split() if w not in stop_words]
+
+    return ' '.join(x)
+
+
+####################################################
+# CREATE A LIST THAT YOU WANT TO VISUALIZE
+####################################################
+list(sl_df)
+#sl_df.shape
+#sl_df.head()
+#sl_df.tail()
+
+#sl_df['sent_clean'] = sl_df['Sentence'].apply(preprocess)
+#sl_df.shape
+#sl_df.head()
+#sl_df.tail()
+#sl_df.count()
+
+print(sl_df['Sentence'])
+sl_df['sent_clean'] = sl_df['Sentence'].apply(preprocess)
+
+####################################################
+# CREATE THE WORD CLOUD
+####################################################
+
+# https://www.datacamp.com/community/tutorials/wordcloud-python
+
+def show_wordcloud(data, title=None):
+    wordcloud = WordCloud(
+        background_color='white',
+        stopwords=stop_words,
+        max_words=200,
+        max_font_size=40,
+        scale=3,
+        random_state=1  # chosen at random by flipping a coin; it was heads
+    ).generate(str(data))
+
+    fig = plt.figure(1, figsize=(12, 12))
+    plt.axis('off')
+    if title:
+        fig.suptitle(title, fontsize=20)
+        fig.subplots_adjust(top=2.3)
+
+    plt.imshow(wordcloud)
+    plt.show()
+
+
+
+show_wordcloud(sl_df['sent_clean'])
