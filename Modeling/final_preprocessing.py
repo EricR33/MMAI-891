927e41eebafb03abca70b2b5193e1cd1741a4a63
EricR33 <59517978+EricR33@users.noreply.github.com>
2020-05-21 16:13:22 -0400

    Final Preprocessing1

--- Modeling/final_preprocessing.py
+++ Modeling/final_preprocessing.py
@@ -1,7 +1,8 @@
 import os
 import textstat
 import nltk
-import re, collections
+import re
+import collections
 from collections import defaultdict
 import unidecode
 # import numpy as np
@@ -18,83 +19,8 @@ from Modeling.final_preprocessing import preprocess
 
 # Regular Expression Cleaning
 def print_string(x):
-    print(x)
-
-def preprocess(x):
-    import re
-
-    import unidecode
-
-    # x = re.sub(r"[abc\\]$", "'", x)        # trying to get rid of \'s notation
-    x = re.sub(r"@CAPS", " ", x)
-    x = re.sub(r"@DATE", " ", x)
-    x = re.sub(r"@LOCATION", " ", x)
-    x = re.sub(r"@ORGANIZATION", " ", x)
-    x = re.sub(r"@NUM", " ", x)
-    x = re.sub(r"@PERCENT", " ", x)
-    x = re.sub(r"@PERSON", " ", x)
-    x = re.sub(r"pERSON", " ", x)               # Chin's spellchecker version
-    x = re.sub(r" mon ", " mom ", x)            # need to fix the mispelled word of mom to mon in spell checked version
-    x = re.sub(r"@MONTH", " ", x)
-    x = re.sub(r"@CITY", " ", x)
-    x = re.sub(r"@YEAR", " ", x)
-    x = re.sub(r"dear", " ", x)
-    x = re.sub(r"local newspaper", " ", x)
-    x = re.sub(r"dear newspaper", " ", x)
-    x = re.sub(r'[^\w\s]', '', x)
-    x = unidecode.unidecode(x)
-    x = re.sub(r'\s+', ' ', x)
-    x = re.sub(r'\d+', '', x)
-    x = x.lower()
-    x = re.sub(r"lifers", "life", x)            # Chin's spellchecker version
-    x = re.sub(r"that's", "that is", x)
-    x = re.sub(r"there's", "there is", x)
-    x = re.sub(r"what's", "what is", x)
-    x = re.sub(r"where's", "where is", x)
-    x = re.sub(r"it's", "it is", x)
-    x = re.sub(r"who's", "who is", x)
-    x = re.sub(r"i'm", "i am", x)
-    x = re.sub(r"she's", "she is", x)
-    x = re.sub(r"he's", "he is", x)
-    x = re.sub(r"they're", "they are", x)
-    x = re.sub(r"who're", "who are", x)
-    x = re.sub(r"you're", "you are", x)
-    x = re.sub(r"ain't", "am not", x)
-    x = re.sub(r"aren't", "are not", x)
-    x = re.sub(r"wouldn't", "would not", x)
-    x = re.sub(r"shouldn't", "should not", x)
-    x = re.sub(r"couldn't", "could not", x)
-    x = re.sub(r"doesn't", "does not", x)
-    x = re.sub(r"isn't", "is not", x)
-    x = re.sub(r"can't", "can not", x)
-    x = re.sub(r"couldn't", "could not", x)
-    x = re.sub(r"won't", "will not", x)
-    x = re.sub(r"i've", "i have", x)
-    x = re.sub(r"you've", "you have", x)
-    x = re.sub(r"they've", "they have", x)
-    x = re.sub(r"we've", "we have", x)
-    x = re.sub(r"don't", "do not", x)
-    x = re.sub(r"didn't", "did not", x)
-    x = re.sub(r"i'll", "i will", x)
-    x = re.sub(r"you'll", "you will", x)
-    x = re.sub(r"he'll", "he will", x)
-    x = re.sub(r"she'll", "she will", x)
-    x = re.sub(r"they'll", "they will", x)
-    x = re.sub(r"we'll", "we will", x)
-    x = re.sub(r"i'd", "i would", x)
-    x = re.sub(r"you'd", "you would", x)
-    x = re.sub(r"he'd", "he would", x)
-    x = re.sub(r"she'd", "she would", x)
-    x = re.sub(r"they'd", "they would", x)
-    x = re.sub(r"we'd", "we would", x)
-    x = re.sub(r"she's", "she has", x)
-    x = re.sub(r"he's", "he has", x)
-    x = re.sub(r"caps", " ", x)               # Chin's spellchecker version
-    x = re.sub(r"location", " ", x)           # Chin's spellchecker version
-    x = re.sub(r"date", " ", x)               # Chin's spellchecker version
-    x = re.sub(r"person", " ", x)             # Chin's spellchecker version
-    x = re.sub(r"organization", " ", x)       # Chin's spellchecker version
     return x
+
 
 
 # Lemmatizer needs to be called after we check for spelling mistakes
