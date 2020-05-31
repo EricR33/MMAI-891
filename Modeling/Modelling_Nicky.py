10fc905a847bda9d6f12288adb330cdf013df7b0
17jw146 <56327499+17jw146@users.noreply.github.com>
2020-05-31 16:46:09 -0400

    Models - Attention included

--- Modeling/Modelling_Nicky.py
+++ Modeling/Modelling_Nicky.py
@@ -7,6 +7,15 @@ Original file is located at
     https://colab.research.google.com/drive/15gpf59pfXNYbtxnLYQS3Y5t5HRZ6t96g
 """
 
+pip
+install
+textstat
+
+!pip
+install
+pyyaml
+h5py
+
 import numpy as np
 import nltk
 import re
@@ -31,8 +40,6 @@ from tensorflow.keras import layers
 import tensorflow_hub as hub
 import random
 import math
-
-##pip install textstat
 
 from google.colab import drive
 
@@ -115,6 +122,13 @@ train_data = batched_dataset.skip(TEST_BATCHES)
 
 next(iter(train_data))
 
+checkpoint_path = os.path.join(DATASET_DIR, "training_1/cp.ckpt")
+checkpoint_dir = os.path.dirname(checkpoint_path)
+
+cp_callback = tf.keras.callbacks.ModelCheckpoint(filepath=checkpoint_path,
+                                                 save_weights_only=True,
+                                                 verbose=1)
+
 
 class TEXT_MODEL(tf.keras.Model):
 
@@ -171,10 +185,24 @@ text_model = TEXT_MODEL(vocabulary_size=VOCAB_LENGTH,
 
 text_model.compile(loss="mean_absolute_error", optimizer="adam", metrics=['mean_absolute_error'])
 
-text_model.fit(train_data, epochs=NB_EPOCHS)
+# Commented out IPython magic to ensure Python compatibility.
+# %%time
+# text_model.fit(train_data, epochs=NB_EPOCHS, callbacks=[cp_callback])
+
+!ls
+{checkpoint_dir}
+
+text_model.load_weights(checkpoint_path)
 
 results = text_model.evaluate(test_data)
 print(results)
+
+checkpoint_path = os.path.join(DATASET_DIR, "training_2/cp.ckpt")
+checkpoint_dir = os.path.dirname(checkpoint_path)
+
+cp_callback = tf.keras.callbacks.ModelCheckpoint(filepath=checkpoint_path,
+                                                 save_weights_only=True,
+                                                 verbose=1)
 
 
 class TEXT_MODEL(tf.keras.Model):
@@ -231,10 +259,27 @@ text_model = TEXT_MODEL(vocabulary_size=VOCAB_LENGTH,
 
 text_model.compile(loss="mean_absolute_error", optimizer="adam", metrics=['mean_absolute_error'])
 
-text_model.fit(train_data, epochs=NB_EPOCHS)
+# Commented out IPython magic to ensure Python compatibility.
+# %%time
+# text_model.fit(train_data, epochs=NB_EPOCHS, callbacks=[cp_callback])
+
+!ls
+{checkpoint_dir}
 
 results = text_model.evaluate(test_data)
 print(results)
+
+text_model.load_weights(checkpoint_path)
+
+results = text_model.evaluate(test_data)
+print(results)
+
+checkpoint_path = os.path.join(DATASET_DIR, "training_3/cp.ckpt")
+checkpoint_dir = os.path.dirname(checkpoint_path)
+
+cp_callback = tf.keras.callbacks.ModelCheckpoint(filepath=checkpoint_path,
+                                                 save_weights_only=True,
+                                                 verbose=1)
 
 
 ###==========================
@@ -292,14 +337,24 @@ text_model = TEXT_MODEL(vocabulary_size=VOCAB_LENGTH,
 
 text_model.compile(loss="mean_absolute_error", optimizer="adam", metrics=['mean_absolute_error'])
 
-text_model.fit(train_data, epochs=NB_EPOCHS)
+# Commented out IPython magic to ensure Python compatibility.
+# %%time
+# text_model.fit(train_data, epochs=NB_EPOCHS, callbacks=[cp_callback])
+
+!ls
+{checkpoint_dir}
+
+results = text_model.evaluate(test_data)
+print(results)
+
+text_model.load_weights(checkpoint_path)
 
 results = text_model.evaluate(test_data)
 print(results)
 
 
 ###==========================
-## manual features attached to the input feeding into NN (please ignore)
+## manual features attached to the input feeding into NN
 ##==========================
 
 class TEXT_MODEL(tf.keras.Model):
@@ -354,3 +409,122 @@ text_model = TEXT_MODEL(vocabulary_size=VOCAB_LENGTH,
 text_model.compile(loss="mean_absolute_error", optimizer="adam", metrics=['mean_absolute_error'])
 
 text_model.fit(train_data, epochs=NB_EPOCHS)
+
+###==========================
+## Attention
+##==========================
+
+
+checkpoint_path = os.path.join(DATASET_DIR, "training_4/cp.ckpt")
+checkpoint_dir = os.path.dirname(checkpoint_path)
+
+cp_callback = tf.keras.callbacks.ModelCheckpoint(filepath=checkpoint_path,
+                                                 save_weights_only=True,
+                                                 verbose=1)
+
+
+class Attention(tf.keras.Model):
+    def __init__(self, units):
+        super(Attention, self).__init__()
+        self.W1 = tf.keras.layers.Dense(units)
+        self.W2 = tf.keras.layers.Dense(units)
+        self.V = tf.keras.layers.Dense(1)
+
+    def call(self, features, hidden):
+        # hidden shape == (batch_size, hidden size)
+        # hidden_with_time_axis shape == (batch_size, 1, hidden size)
+        # we are doing this to perform addition to calculate the score
+        hidden_with_time_axis = tf.expand_dims(hidden, 1)
+
+        # score shape == (batch_size, max_length, 1)
+        # we get 1 at the last axis because we are applying score to self.V
+        # the shape of the tensor before applying self.V is (batch_size, max_length, units)
+        score = tf.nn.tanh(
+            self.W1(features) + self.W2(hidden_with_time_axis))
+        # attention_weights shape == (batch_size, max_length, 1)
+        attention_weights = tf.nn.softmax(self.V(score), axis=1)
+
+        # context_vector shape after sum == (batch_size, hidden_size)
+        context_vector = attention_weights * features
+        context_vector = tf.reduce_sum(context_vector, axis=1)
+        return context_vector, attention_weights
+
+
+class TEXT_MODEL(tf.keras.Model):
+
+    def __init__(self,
+                 vocabulary_size,
+                 embedding_dimensions=100,
+                 embedding_matrix=embedding_matrix,
+                 dnn_units=256,
+                 dropout_rate=0.1,
+                 training=False,
+                 name="text_model"):
+        super(TEXT_MODEL, self).__init__(name=name)
+
+        self.embedding = layers.Embedding(vocabulary_size, embedding_dimensions, weights=[embedding_matrix],
+                                          # using the pre-trained embedding
+                                          ##input_length=max_len,
+                                          trainable=False)
+
+        ##self.rnn_layer1 = layers.LSTM(256) ##, input_shape=(None, 1))
+        self.rnn_layer2 = layers.Bidirectional(layers.LSTM(32, return_sequences=True), name="bi_lstm_0")
+        self.rnn_layer3 = layers.Bidirectional(layers.LSTM(32, return_sequences=True, return_state=True),
+                                               name="bi_lstm_1")
+        self.concat = layers.Concatenate(axis=-1)
+        self.attention = Attention(10)
+
+        self.dense_1 = layers.Dense(units=dnn_units, activation="relu")
+        self.dropout = layers.Dropout(rate=dropout_rate)
+        self.last_dense = layers.Dense(units=1, activation="linear")
+
+    def call(self, inputs, training):
+        nn_input = tf.slice(inputs, [0, 13], [-1, -1])
+        l = self.embedding(nn_input)
+        ##nn_input = tf.expand_dims(nn_input, -1)
+        l_1 = self.rnn_layer2(l)
+        (lstm, forward_h, forward_c, backward_h, backward_c) = self.rnn_layer3(l_1)
+        state_h = self.concat([forward_h, backward_h])
+        state_c = self.concat([forward_c, backward_c])
+        context_vector, attention_weights = self.attention(lstm, state_h)
+
+        feature_input = tf.slice(inputs, [0, 0], [-1, 13])
+        feature_input = tf.cast(feature_input, dtype=tf.float32)
+        concatenated = tf.concat([context_vector, feature_input], axis=-1)
+        concatenated = self.dense_1(concatenated)
+        concatenated = self.dropout(concatenated, training)
+        model_output = self.last_dense(concatenated)
+
+        return model_output
+
+
+VOCAB_LENGTH = len(word_index) + 1
+EMB_DIM = 100
+DNN_UNITS = 256
+DROPOUT_RATE = 0.2
+NB_EPOCHS = 20
+max_len = 861
+
+text_model = TEXT_MODEL(vocabulary_size=VOCAB_LENGTH,
+                        embedding_dimensions=EMB_DIM,
+                        embedding_matrix=embedding_matrix,
+                        ##max_len = max_len,
+                        dnn_units=DNN_UNITS,
+                        dropout_rate=DROPOUT_RATE)
+
+text_model.compile(loss="mean_absolute_error", optimizer="adam", metrics=['mean_absolute_error'])
+
+# Commented out IPython magic to ensure Python compatibility.
+# %%time
+# text_model.fit(train_data, epochs=NB_EPOCHS, callbacks=[cp_callback])
+
+!ls
+{checkpoint_dir}
+
+results = text_model.evaluate(test_data)
+print(results)
+
+text_model.load_weights(checkpoint_path)
+
+results = text_model.evaluate(test_data)
+print(results)
